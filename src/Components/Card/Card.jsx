634659be795b0cb8f74bd9cb63d927601b7130b6
Fahid21 <mdfahidhasn871@gmail.com>
2023-03-31 19:17:27 +0600

    fourth done

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Card = ({ card }) => {
+const Card = (props ) => {
+    const {card} =props;
+    console.log(card);
+  
+
+    let total = 0;
+for (const vlog of card){
+    total = total + parseInt(vlog.readingtime);
+    
+};
+
   return (
-    <div>
+    <div className="sticky top-4">
       <div className="border-violet-800 bg-violet-200 ">
         <p className=" p-3 text-lg text-center text-violet-700 font-medium text-#6047EC">
-          Spent time on read :{} min
+          Spent time on read :{total} min
         </p>
       </div>
 
@@ -13,6 +23,7 @@ const Card = ({ card }) => {
         <p className="font-semibold text-2xl text-center bg-zinc-200 mt-5">
           Bookmarked Blogs :{card.length}
         </p>
+        <p>{card.length}</p>
       </div>
     </div>
   );
